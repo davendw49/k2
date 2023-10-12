80edb4243c3c46abbed97bcc38111c238d5943cd
daven <davendw@sjtu.edu.cn>
2023-10-13 00:43:08 +0800

    add eval

---
+++ evaluation/post_process.py
@@ -0,0 +1,269 @@
+import json
+import re
+import numpy as np
+import pickle
+import math
+import argparse
+
+token_setup = {
+    "llama": {
+        'A': 319,
+        'B': 350,
+        'C': 315,
+        'D': 360,
+        'E': 382,
+        'F': 383,
+        'G': 402,
+        'H': 379,
+        'a': 263,
+        'b': 289,
+        'c': 274,
+        'd': 270,
+        'e': 321,
+        'f': 285,
+        'g': 330,
+        'h': 298,
+        'true': 1565,
+        'false': 2089,
+        'True': 5852,
+        'False': 7700
+    },
+    "gpt2": {
+        'A': 32,
+        'B': 33,
+        'C': 34,
+        'D': 35,
+        'E': 36,
+        'F': 37,
+        'G': 38,
+        'H': 39,
+        'a': 64,
+        'b': 65,
+        'c': 66,
+        'd': 67,
+        'e': 68,
+        'f': 69,
+        'g': 70,
+        'h': 71,
+        'true': 7942,
+        'false': 9562,
+        'True': 17821,
+        'False': 25101
+    },
+    "mpt": {
+        'A': 34,
+        'B': 35,
+        'C': 36,
+        'D': 37,
+        'E': 38,
+        'F': 39,
+        'G': 40,
+        'H': 41,
+        'a': 66,
+        'b': 67,
+        'c': 68,
+        'd': 69,
+        'e': 70,
+        'f': 71,
+        'g': 72,
+        'h': 73,
+        'true': 5672,
+        'false': 7750,
+        'True': 5088,
+        'False': 5653
+    },
+    "gal": {
+        'A': 55,
+        'B': 56,
+        'C': 57,
+        'D': 58,
+        'E': 59,
+        'F': 60,
+        'G': 61,
+        'H': 62,
+        'a': 87,
+        'b': 88,
+        'c': 89,
+        'd': 90,
+        'e': 91,
+        'f': 92,
+        'g': 93,
+        'h': 94,
+        'true': 11852,
+        'false': 18362,
+        'True': 8409,
+        'False': 10394
+    }
+}
+base_benchmark_path = r'./benchmarks/'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_name', '-m', help='name of the model, should be unique', required=True)
+parser.add_argument('--benchmark_name', '-b', help='name of the benchmark, should be unique', required=True)
+args = parser.parse_args()
+
+model = args.model_name
+benchmark = args.benchmark_name
+
+if "k2" in model or "K2" in model or "llama" in model or "vicuna" in model or "alpaca" in model:
+    print('using llama tokenizer')
+    token2id = token_setup['llama']
+elif "mpt" in model:
+    print('using mpt tokenizer')
+    token2id = token_setup['mpt']
+elif "gpt" in model:
+    print('using gpt tokenizer')
+    token2id = token_setup['gpt2']
+elif "gal" in model:
+    print('using gal tokenizer')
+    token2id = token_setup['gal']
+
+def extract_choice_id_ls(text: str):
+    pat = r'(?<=[^\w\-])[A-Z](?=[^\w\-])'
+    match = re.findall(pat, text)
+    cap_ls = np.unique(sorted([ord(i)-ord('A') for i in match]))
+    maximum = 0
+    for i in range(0, min(len(cap_ls), 26)):
+        if cap_ls[i] == i:
+            maximum += 1
+    return [token2id[chr(i + ord('A'))] for i in range(0, maximum)]
+
+def process_tf(filepath, softmax):
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+    key = 'tf'
+    question_ls = []
+    answer_ls = []
+    for i in range(len(data[key]["question"])):
+        q = data[key]["question"][i]
+        a = data[key]['answer'][i]
+        question_ls.append(q)
+        answer_ls.append(a)
+    
+    tf_res = []
+    for i in range(len(question_ls)):
+        prob_ls = []
+        tf_id_ls = [token2id['True'], token2id['False']]
+        for j in range(len(tf_id_ls)):
+            c_id = tf_id_ls[j]
+            prob_ls.append(softmax[i].tolist()[c_id])
+        prob_ls_normalized = [i/(sum(prob_ls)) for i in prob_ls]
+        entropy = -sum([i * math.log(i,2) for i in prob_ls_normalized])
+        tf_res.append({
+            'answer': answer_ls[i],
+            'probability_distribution': prob_ls,
+            'normalized': prob_ls_normalized,
+            'normalized_entropy': entropy,
+        })
+
+    correct = 0
+    total = 0
+    for i in tf_res:
+        ans = 0 if i['answer'] == 'True' else 1
+        total += 1
+        norm = np.array(i['normalized'])
+        if ans == norm.argmax():
+            correct += 1
+    return tf_res, correct / total
+
+def process_choice(filepath, softmax):
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+    if 'npee' in filepath or 'kaoyan' in filepath or 're' in filepath:
+        key = 'choice'
+        question_ls = []
+        answer_ls = []
+        for i in range(len(data[key]["question"])):
+            q = data[key]["question"][i]
+            a = data[key]['answer'][i]
+            question_ls.append(q)
+            answer_ls.append(a)
+        
+        choice_res = []
+        choice_num_ls = []
+        for i in range(len(question_ls)):
+            prob_ls = []
+            question = question_ls[i]
+            choice_id_ls = extract_choice_id_ls(question) # capital letters only
+            choice_num_ls.append(len(choice_id_ls))
+            for j in range(len(choice_id_ls)):
+                c_id = choice_id_ls[j]
+                prob_ls.append(softmax[i].tolist()[c_id])
+            prob_ls_normalized = [i/(sum(prob_ls)) for i in prob_ls]
+            entropy = -sum([i * math.log(i,2) for i in prob_ls_normalized])
+            choice_res.append({
+                'answer': answer_ls[i],
+                'probability_distribution': prob_ls,
+                'normalized': prob_ls_normalized,
+                'normalized_entropy': entropy,
+            })
+    else:
+        question_ls = []
+        answer_ls = []
+        for item in data:
+            q = item["question"]
+            a = item['answerKey']
+            question_ls.append(q)
+            answer_ls.append(a)
+        
+        choice_res = []
+        choice_num_ls = []
+        for i in range(len(question_ls)):
+            prob_ls = []
+            question = question_ls[i]
+            choice_id_ls = [token2id[cho['label']] for cho in question['choices']] # capital letters only
+            choice_num_ls.append(len(choice_id_ls))
+            for j in range(len(choice_id_ls)):
+                c_id = choice_id_ls[j]
+                prob_ls.append(softmax[i].tolist()[c_id])
+            prob_ls_normalized = [i/(sum(prob_ls)) for i in prob_ls]
+            entropy = -sum([i * math.log(i,2) for i in prob_ls_normalized])
+            choice_res.append({
+                'answer': answer_ls[i],
+                'probability_distribution': prob_ls,
+                'normalized': prob_ls_normalized,
+                'normalized_entropy': entropy,
+            })
+        
+
+    correct = 0
+    random_correct_expectation = 0
+    total = 0
+    for i in range(len(choice_res)):
+        try:
+            ans = ord(choice_res[i]['answer']) - ord('A')
+        except:
+            continue
+        total += 1
+        norm = np.array(choice_res[i]['normalized'])
+        if ans == norm.argmax():
+            correct += 1
+        random_correct_expectation += 1 / choice_num_ls[i]
+    return choice_res, correct / total, random_correct_expectation / total
+
+
+print('Start to evaluate ...')
+with open(f'./pickle/softmax_{model}_{benchmark}.pickle', 'rb') as f:
+    softmax = pickle.load(f)
+
+if 'npee' in benchmark or 'kaoyan' in benchmark:
+    choice_softmax_wa = softmax['choice']['wa'] if 'choice' in softmax else []
+    choice_softmax_woa = softmax['choice']['woa'] if 'choice' in softmax else []
+    choice_res_wa, correct_rate_wa, correct_rate_random = process_choice(f'{base_benchmark_path}benchmark_{benchmark}.json', choice_softmax_wa)
+    choice_res_woa, correct_rate_woa, correct_rate_random = process_choice(f'{base_benchmark_path}benchmark_{benchmark}.json', choice_softmax_woa)
+    with open(f"./results/{model.replace('-', '_')}_result_on_{benchmark}.txt", 'w') as f:
+        f.write(f'Correct rate (choice, with "the answer is ", {model}) is {correct_rate_wa}' + '\n')
+        f.write(f'Correct rate (choice, without "the answer is ", {model}) is {correct_rate_woa}' + '\n')
+        f.write(f'Correct rate (choice, random) is {correct_rate_random}' + '\n')
+
+if 'npee' in benchmark or 'kaoyan' in benchmark or 're' in benchmark:
+    tf_softmax_wa = softmax['tf']['wa'] if 'tf' in softmax else []
+    tf_softmax_woa = softmax['tf']['woa'] if 'tf' in softmax else []
+    tf_res_wa, tf_correct_rate_wa= process_tf(f'{base_benchmark_path}benchmark_{benchmark}.json', tf_softmax_wa)
+    tf_res_woa, tf_correct_rate_woa= process_tf(f'{base_benchmark_path}benchmark_{benchmark}.json', tf_softmax_woa)
+    with open(f"./results/{model.replace('-', '_')}_result_on_{benchmark}.txt", 'a') as f:
+        f.write(f'Correct rate (tf, with "the answer is ", {model}) is {tf_correct_rate_wa}' + '\n')
+        f.write(f'Correct rate (tf, without "the answer is ", {model}) is {tf_correct_rate_woa}' + '\n')
+        f.write(f'Correct rate (tf, random) is 0.5' + '\n')
+
+print('Finished!')
---
+++ evaluation/run_eval.py
@@ -0,0 +1,233 @@
+# Code adapted from: https://github.com/zsc/llama_infer
+# All copyrights belong to the original code owner.
+import json
+
+import torch
+import argparse
+import threading
+import sys
+from accelerate import init_empty_weights, infer_auto_device_map
+import transformers
+from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM
+
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer, LlamaModel, OPTForCausalLM
+from transformers import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
+
+from peft import PeftModel
+from typing import List, Union
+from tqdm import tqdm
+import re
+import numpy as np
+import pickle
+import torch.nn.functional as F
+import os
+
+
+def get_device_map(model_name, device, do_int8):
+
+    with init_empty_weights():
+        config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
+        model = AutoModelForCausalLM.from_config(config, trust_remote_code=True)
+
+    d = {device: "24GiB"}
+    # for i in range(1, 4):
+    #     d[i] = "24GiB"
+    device_map = infer_auto_device_map(
+        model, max_memory=d, dtype=torch.int8 if do_int8 else torch.float16,
+        no_split_module_classes=["BloomBlock", "OPTDecoderLayer", "LlamaDecoderLayer"]
+    )
+    print(device_map)
+    del model
+    return device_map
+
+def run_generate(input):
+    # prompt = f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{input}\n\n### Response:\n{x1}"
+    generate_kwargs = {
+        "max_new_tokens": 700,
+        "min_new_tokens": 10,
+        "temperature": 0.2,
+        "do_sample": True,  # The three options below used together leads to contrastive search
+        "top_p": 0.75,
+        "top_k": 40,
+        "num_beams": 4,
+        "penalty_alpha": 1.1,
+    }
+    with torch.no_grad():
+        input_ids = tokenizer(input, return_tensors="pt", padding=True).input_ids
+        assert len(input_ids) == 1, len(input_ids)
+        if input_ids[0][-1] == 2:  # 2 is EOS, hack to remove. If the prompt is ending with EOS, often the generation will stop abruptly.
+            input_ids = input_ids[:, :-1]
+        input_ids = input_ids.to("cpu")
+
+        generated_ids = model.generate(
+            input_ids,
+            **generate_kwargs
+        )
+
+        result = tokenizer.batch_decode(generated_ids.cpu(), skip_special_tokens=True)
+        return result[0].strip()[len(input):].strip()
+
+if __name__ == "__main__":   
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", '-n', help='name of the model, should be unique', required=True)
+    parser.add_argument("--base_model", '-p', help='path of the model', required=True)
+    parser.add_argument("--lora_weights", '-l', help='path of the lora weights', default=None)
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--do_int8", action="store_true")
+    parser.add_argument("--low_cpu_mem_usage", action="store_true")
+    parser.add_argument("--port", type=int, default=12333)
+    parser.add_argument("--no_cuda", action="store_true")
+    parser.add_argument("--geobenchmark", type=str, default="npee")
+    args = parser.parse_args()
+    
+    model_name = args.model_name
+    base_model = args.base_model
+    lora_weights = args.lora_weights
+    device = args.device
+    
+    print('Import finished...')
+    print(f'Model name: {model_name}')
+    print(f'Model path: {base_model}')
+    print(f'LoRA weights path: {lora_weights}')
+    
+    if base_model == "gpt2-xl":
+        tokenizer = GPT2Tokenizer.from_pretrained(args.base_model)
+        model = GPT2LMHeadModel.from_pretrained(
+            args.base_model,
+            device_map=get_device_map(args.base_model, args.device, args.do_int8) if not args.no_cuda else None,
+            torch_dtype=torch.int8 if args.do_int8 else torch.float16 if not args.no_cuda else torch.float32,
+            load_in_8bit=args.do_int8,
+        )
+    elif base_model == "llama-7b":
+        tokenizer = LlamaTokenizer.from_pretrained("/home/daven/llm/qokori/llama-7b")
+        model = LlamaForCausalLM.from_pretrained(
+            "/home/daven/llm/qokori/llama-7b",
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
+            device_map=get_device_map("huggyllama/llama-7b", args.device, args.do_int8) if not args.no_cuda else None,
+        )
+    elif base_model == "llama-13b":
+        tokenizer = LlamaTokenizer.from_pretrained("huggyllama/llama-13b")
+        model = LlamaForCausalLM.from_pretrained(
+            "huggyllama/llama-13b",
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
+            device_map=get_device_map("huggyllama/llama-13b", args.device, args.do_int8) if not args.no_cuda else None,
+        )
+    elif base_model == "mpt-7b":
+        tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+        model = AutoModelForCausalLM.from_pretrained(
+            "mosaicml/mpt-7b",
+            trust_remote_code=True,
+            device_map=get_device_map("mosaicml/mpt-7b", args.device, args.do_int8) if not args.no_cuda else None,
+            torch_dtype=torch.float16,
+        )
+    elif base_model == "gal-6.7b":
+        tokenizer = AutoTokenizer.from_pretrained("facebook/galactica-6.7b")
+        model = OPTForCausalLM.from_pretrained(
+            "facebook/galactica-6.7b", 
+            device_map=get_device_map("facebook/galactica-6.7b", args.device, args.do_int8) if not args.no_cuda else None, 
+            torch_dtype=torch.float16,
+        )
+    elif base_model == "vicuna-7b":
+        tokenizer = AutoTokenizer.from_pretrained("/home/daven/llm/FastChat/vicuna-7b")
+        model = OPTForCausalLM.from_pretrained(
+            "/home/daven/llm/FastChat/vicuna-7b", 
+            device_map=get_device_map("/home/daven/llm/FastChat/vicuna-7b", args.device, args.do_int8) if not args.no_cuda else None, 
+            torch_dtype=torch.float16,
+        )
+    elif base_model == "alpaca-7b":
+        tokenizer = LlamaTokenizer.from_pretrained("chainyo/alpaca-lora-7b")
+        model = LlamaForCausalLM.from_pretrained(
+            "chainyo/alpaca-lora-7b",
+            load_in_8bit=True,
+            device_map=get_device_map("chainyo/alpaca-lora-7b", args.device, args.do_int8) if not args.no_cuda else None, 
+            torch_dtype=torch.float16,
+        )
+    else:
+        """
+        K2 (GeoLLaMA) series Models
+        """
+        if lora_weights is None:
+            load_8bit = True
+            tokenizer = LlamaTokenizer.from_pretrained(base_model)
+            model = LlamaForCausalLM.from_pretrained(
+                base_model,
+                load_in_8bit=load_8bit,
+                device_map=get_device_map(args.base_model, args.device, args.do_int8) if not args.no_cuda else None,
+                torch_dtype=torch.float16
+            )
+            model.config.pad_token_id = tokenizer.pad_token_id = 0
+            model.config.bos_token_id = 1
+            model.config.eos_token_id = 2
+            
+            if not load_8bit:
+                model.half()
+                print("We need to make the model smaller")
+        else:
+            load_8bit = False
+            tokenizer = LlamaTokenizer.from_pretrained(base_model)
+            model = LlamaForCausalLM.from_pretrained(
+                base_model,
+                load_in_8bit=load_8bit,
+                device_map=get_device_map(args.base_model, args.device, args.do_int8) if not args.no_cuda else None,
+                torch_dtype=torch.float16
+            )
+            model = PeftModel.from_pretrained(
+                model,
+                lora_weights,
+                torch_dtype=torch.float16,
+                device_map=get_device_map(args.base_model, args.device, args.do_int8) if not args.no_cuda else None,
+            )
+            model.config.pad_token_id = tokenizer.pad_token_id = 0
+            model.config.bos_token_id = 1
+            model.config.eos_token_id = 2
+
+            if not load_8bit:
+                model.half()
+            model.eval()
+            if torch.__version__ >= "2" and sys.platform != "win32":
+                model = torch.compile(model)
+    # tokenizer.pad_token_id = -1
+    print('Model loaded.')
+    
+    with open(f'./benchmarks/source_target_{args.geobenchmark}.json', 'r') as f:
+        source_target = json.load(f)
+
+    
+    all_softmax = {}
+    for question_type in ['choice', 'tf']:
+        if question_type == 'choice' and args.geobenchmark == 're':
+            continue
+        print(f"Task: {'True-False' if question_type == 'tf' else 'Multiple Choice'}")
+        question_data = {}
+        for the_answer_is in ['wa', 'woa']:
+            print(f"Generate the answer with{'' if the_answer_is == 'wa' else 'out'}")
+            softmax = []
+            source = source_target['source'][question_type][the_answer_is]
+            for i in tqdm(range(len(source))):
+                input = source[i]
+                input_ids = tokenizer(input, return_tensors='pt')
+                outputs = model(input_ids["input_ids"])
+                softmax_res = F.softmax(outputs["logits"][0][-1].float()).detach()
+                softmax.append(softmax_res)
+            question_data[the_answer_is] = softmax
+        all_softmax[question_type] = question_data
+            
+    with open(f'./pickle/softmax_{model_name}_{args.geobenchmark}.pickle', 'wb') as f:
+        pickle.dump(all_softmax, f)
+        
+    os.system(f'python ./post_process.py -m {model_name} -b {args.geobenchmark}')
+    
+"""
+Here is an example:
+
+- For k2 series model with lora
+> python run_eval.py --model_name k2_ni --base_model /home/daven/llm/qokori/llama-2023-05-07-15-10/checkpoint-19180/ --lora_weights /home/daven/llm/qokori/qokori-sft/outputs/geo_llama_ni/
+
+- For k2 series model without lora
+> python run_eval.py --model_name geollama_19180 --base_model /home/daven/llm/qokori/llama-2023-05-07-15-10/checkpoint-19180/
+
+- For non-k2 series model
+> python run_eval.py --model_name gpt2_xl --base_model gpt2-xl
+"""
